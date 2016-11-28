8daa4a88b8ab9453a0f76e0cdb42562f8241e43d
shenrui <shenrui@cn.ibm.com>
2016-11-28 13:27:18 +0800

    automatically get book name and download mp3 into book name folder

--- personal/baobao88/baobao88down.py
+++ personal/baobao88/baobao88down.py
@@ -11,30 +11,39 @@ import urllib
 import urllib2
 from urllib2 import HTTPError
 
-cookie=""
+cookie = ""
+
 
 def login():
     global cookie
-    url="http://www.baobao88.com/member/index_do4.php"
-    username='ilovepumpkin'
-    password='passw0rd'
-    http= httplib2.Http(".cache")
-    headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8','Cookie':cookie}
-    body={'fmdo':'login','dopost':'login','userid':username,'pwd':password}
-    resp,content=http.request(url, 'POST',headers=headers, body=urllib.urlencode(body))
-    setcookie=resp['set-cookie']
-    PHPSESSID=re.compile('PHPSESSID=[\w\d]+;').findall(setcookie)[0]
-    DedeUserID=re.compile('DedeUserID=\d+;').findall(setcookie)[0]
-    DedeUserIDckMd5=re.compile('DedeUserIDckMd5=[\w\d]+;').findall(setcookie)[1]
-    DedeLoginTime=re.compile('DedeLoginTime=\d+;').findall(setcookie)[0]
-    DedeLoginTimeckMd5=re.compile('DedeLoginTimeckMd5=[\w\d]+;').findall(setcookie)[1]
-    cookie=PHPSESSID+DedeUserID+DedeUserIDckMd5+DedeLoginTime+DedeLoginTimeckMd5+'AJSTAT_ok_pages=6; AJSTAT_ok_times=1; bdshare_firstime=1372830511943;acopendivids=jason; acgroupswithpersist=pets'
- 
+    url = "http://www.baobao88.com/member/index_do4.php"
+    username = 'ilovepumpkin'
+    password = 'passw0rd'
+    http = httplib2.Http(".cache")
+    headers = {'Content-Type':
+        'application/x-www-form-urlencoded;charset=UTF-8', 'Cookie': cookie}
+    body = {'fmdo': 'login', 'dopost': 'login',
+        'userid': username, 'pwd': password}
+    resp, content = http.request(
+        url, 'POST', headers=headers, body=urllib.urlencode(body))
+    setcookie = resp['set-cookie']
+    PHPSESSID = re.compile('PHPSESSID=[\w\d]+;').findall(setcookie)[0]
+    DedeUserID = re.compile('DedeUserID=\d+;').findall(setcookie)[0]
+    DedeUserIDckMd5 = re.compile(
+        'DedeUserIDckMd5=[\w\d]+;').findall(setcookie)[1]
+    DedeLoginTime = re.compile('DedeLoginTime=\d+;').findall(setcookie)[0]
+    DedeLoginTimeckMd5 = re.compile(
+        'DedeLoginTimeckMd5=[\w\d]+;').findall(setcookie)[1]
+    cookie = PHPSESSID+DedeUserID+DedeUserIDckMd5+DedeLoginTime+DedeLoginTimeckMd5 + \
+        'AJSTAT_ok_pages=6; AJSTAT_ok_times=1; bdshare_firstime=1372830511943;acopendivids=jason; acgroupswithpersist=pets'
+
+
 def httpget(url):
-    http= httplib2.Http(".cache")
-    headers={'Cookie':cookie}
-    resp,content=http.request(url, 'GET',headers=headers)
+    http = httplib2.Http(".cache")
+    headers = {'Cookie': cookie}
+    resp, content = http.request(url, 'GET', headers=headers)
     return content
+
 
 def get_mp3_url(url):
     opener = urllib2.build_opener()
@@ -42,84 +51,107 @@ def get_mp3_url(url):
     f = opener.open(url)
     return f.geturl()
 
-def httppost(url,body):
-    http= httplib2.Http(".cache")
+
+def httppost(url, body):
+    http = httplib2.Http(".cache")
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    response, content = http.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
+    response, content = http.request(
+        url, 'POST', headers=headers, body=urllib.urlencode(body))
     return content.decode('utf-8')
 
-def go(catUrl,catName):
-	last_page_file=catName+'/'+'.last_page'	
-    	last_item_file=catName+'/'+'.last_item'
-    	done_file=catName+'/'+'.done'
-	failed_file=catName+'/'+'.failed'
 
-	print 'Start downloading the category <'+catName+'>'
-    	if not os.path.exists(catName):
-       		os.system('mkdir -p "'+catName+'"')
-    	elif os.path.exists(done_file):
-       		print catName+" was done.Skip it."
-       		return
-    	else:
-       		# delete the newest one(it might be broken) so redownload it.
-       		os.system('more '+last_item_file+' | xargs rm -rf')
+def go(catUrl, catName):
+    last_page_file = catName+'/'+'.last_page'
+    last_item_file = catName+'/'+'.last_item'
+    done_file = catName+'/'+'.done'
+    failed_file = catName+'/'+'.failed'
 
-    	last_page=''
-	last_page_found=True
-    	if os.path.exists(last_page_file):
-		last_page=open(last_page_file).readline().replace('\n','')
-		last_page_found=False
-		print 'The last page is: '+last_page
+    print 'Start downloading the category <'+catName+'>'
+    if not os.path.exists(catName):
+        os.system('mkdir -p "'+catName+'"')
+    
+    if os.path.exists(done_file):
+        print catName+" was done.Skip it."
+        return
+    else:
+        # delete the newest one(it might be broken) so redownload it.
+        os.system('more '+last_item_file+' | xargs rm -rf')
+        last_page = ''
+        last_page_found = True
+        if os.path.exists(last_page_file):
+            last_page = open(last_page_file).readline().replace('\n', '')
+            last_page_found = False
+            print 'The last page is: '+last_page
 
-    	try:
-    		for i in range(1000):
-			j=i+1
-			url=catUrl.replace('.html',str(j)+".html")
+        try:
+            for i in range(1000):
+                j = i+1
+                url = catUrl.replace('.html', str(j)+".html")
 
-			if not last_page_found:
-				if last_page!=url:
-					continue
-				else:
-					print 'Last page found! - '+url
-					last_page_found=True
+                if not last_page_found:
+                    if last_page != url:
+                        continue
+                    else:
+                        print 'Last page found! - '+url
+                        last_page_found = True
 
-			html=httpget(url)
-			html=html.decode('gbk')
+                html = httpget(url)
+                html = html.decode('gbk')
 
-			entries=re.compile('<input name="id\[\]" type="checkbox" value="(\d+)"  onclick="getcheck\(\'id\[\]\'\)"/><a href="[/\w\d]*.html" target="_blank">([^<>"]*)</a>').findall(html)
-			if(len(entries)==0):
-				print '[End]'+url
-				break;
-	   
-			print '[page'+str(j)+']'+url
-			cmd_record_lastpage='echo "'+url+'">'+last_page_file
-			os.system(cmd_record_lastpage)
+                entries = re.compile(
+                    '<input name="id\[\]" type="checkbox" value="(\d+)"  onclick="getcheck\(\'id\[\]\'\)"/><a href="([/\w\d]*.html)" target="_blank">([^<>"]*)</a>').findall(html)
+                if(len(entries) == 0):
+                    print '[End]'+url
+                    break;
 
-			for entry in entries:
-				aId=entry[0]
-				title=entry[1]
-				mp3path=catName.decode('utf-8')+"/"+title+".mp3"
+                print '[page'+str(j)+']'+url
+                cmd_record_lastpage = 'echo "'+url+'">'+last_page_file
+                os.system(cmd_record_lastpage)
 
-				if os.path.exists(mp3path):
-					print mp3path+' exists. Skip downloading it.'
-					continue
+                for entry in entries:
+                    aId = entry[0]
+                    storyPageUrl = 'http://www.baobao88.com'+entry[1]
+                    title = entry[2]
+                    mp3path = catName.decode('utf-8')
 
-				dUrl='http://www.baobao88.com/member/loginsta_DOWN.php?id='+aId+"&tit="+urllib2.quote(title.encode('gbk'));	
-				try:
-					dUrl=get_mp3_url(dUrl)
-				except HTTPError as e:
-				        os.system('echo "['+title.encode('utf-8')+']'+dUrl.encode('utf-8')+'">>'+failed_file)	
-					continue	
-				wgetcmd_mp3='wget --retry-connrefused -O "'+mp3path.encode('utf-8')+'" '+dUrl
-				wgetcmd_mp3='echo "'+mp3path.encode('utf-8')+'">'+last_item_file+';'+wgetcmd_mp3
-				os.system(wgetcmd_mp3)
-		
-		os.system('touch '+done_file)
-     	except Exception as e:	
-		print 'Error occurred: ',e
-		traceback.print_exc()
-		sys.exit(0)
-	
+                    # get the book name
+                    bookName = ""
+                    html = httpget(storyPageUrl)
+                    # html = html.decode('gbk')
+                    html = unicode(html,'gbk')
+                    # entries = re.compile(
+                    #     '<div class="t_mp3_info t_mp3_gc">.*《(.*)》.*</div>').findall(html)
+                    entries = re.compile(u'《(.*)》').findall(html)
+                    if(len(entries) > 0):
+                        bookName = entries[0]
+                        mp3path=mp3path.encode('utf-8')+"/"+bookName.encode('utf-8')
+                        if not os.path.exists(mp3path):
+                            os.system('mkdir -p "'+mp3path+'"')
+
+                    mp3path = mp3path.decode('utf-8')+"/"+title+".mp3"
+
+                    if os.path.exists(mp3path):
+                        print mp3path+' exists. Skip downloading it.'
+                        continue
+
+                    dUrl = 'http://www.baobao88.com/member/loginsta_DOWN.php?id=' + \
+                        aId+"&tit="+urllib2.quote(title.encode('gbk'));
+                    try:
+                        dUrl = get_mp3_url(dUrl)
+                    except HTTPError as e:
+                            os.system(
+                                'echo "['+title.encode('utf-8')+']'+dUrl.encode('utf-8')+'">>'+failed_file)
+                            continue    
+                    wgetcmd_mp3='wget --retry-connrefused -O "'+mp3path.encode('utf-8')+'" '+dUrl
+                    wgetcmd_mp3='echo "'+mp3path.encode('utf-8')+'">'+last_item_file+';'+wgetcmd_mp3
+                    os.system(wgetcmd_mp3)
+            
+            os.system('touch '+done_file)
+        except Exception as e:  
+            print 'Error occurred: ',e
+            traceback.print_exc()
+            sys.exit(0)
+    
 
 def handleHomepageByMainCatName(catNameList):
     html=httpget('http://www.jinmiao.cn').encode('utf-8')
@@ -176,7 +208,7 @@ def handleCatPage(catUrl,catName):
         for link in links:
             fulllink='http://www.jinmiao.cn/'+link[0]
             name=link[1]
-            #name=name[1:len(name)]
+            # name=name[1:len(name)]
             handleDownloadPage(fulllink,name,catName)            
     os.system('touch "'+catName+'/.done"')
 
@@ -234,8 +266,8 @@ os.system('wget -O 测试.mp3 '+dUrl)
 # catList=[["http://www.baobao88.com/list/68/15--0--0--.html",'7-12岁儿童故事'],["http://www.baobao88.com/list/131/23--0--0--.html","小学课文朗读一年级"],["http://www.baobao88.com/bbmusic/baike/95_.html","儿童百科"],["http://www.baobao88.com/youshen/chengyu/99_.html","成语故事"],["http://www.baobao88.com/youshen/shangxiawuqiannian/100_.html","上下五千年"],["http://www.baobao88.com/youshen/bbstory/zuowen/134_.html","小学生作文"]]
 # catList=[["http://www.baobao88.com/babybook/wenxue/zuowen/91_.html","小学作文"],["http://www.baobao88.com/babybook/baike/shenghuo/40_.html","生活百科"],["http://www.baobao88.com/babybook/baike/zhiran/43_.html","自然百科"],["http://www.baobao88.com/babybook/baike/jiankang/41_.html","健康百科"],["http://www.baobao88.com/babybook/baike/anquan/","安全百科"]]
 
-catList=[["http://www.baobao88.com/list/101/15--182--136--.html",'7-12岁/国外作品/生活小说'],['']]
+catList=[["http://www.baobao88.com/list/101/15--182--137--.html",'7-12岁/国外作品/童话小说']]
 
 login()
 for cat in catList:
-	go(cat[0],cat[1])
+    go(cat[0],cat[1])
