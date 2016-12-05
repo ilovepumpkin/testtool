3e3dbc35697fb0d4993409be6f6d6c94ef21942f
shenrui <shenrui@cn.ibm.com>
2016-12-05 10:01:36 +0800

    improve

--- personal/baobao88/auto_group.py
+++ personal/baobao88/auto_group.py
@@ -19,10 +19,10 @@ def findCommStr(str1,str2):
         if str1[:i]!=str2[:i]:
             break
         i+=1  
-    if i==1:
+    if i<4:
         return None
     else:
-        return re.compile('\d*$').sub('',str1[:i-1])
+        return re.compile('[\d_]*$').sub('',str1[:i-1])
 
 def validate(name):
     if name.startswith('第'): 
@@ -35,7 +35,10 @@ def validate(name):
 def group():
     folders=[]
     for root,dirs,files in os.walk(path,True):
-        print dirs
+        # only handle the first level
+        if root!=path: 
+            break
+
         folders.extend(map(lambda x:x.decode('utf8'),dirs))
         i=0
         files=sorted(files)
@@ -43,30 +46,39 @@ def group():
         while i<file_count:
             # print files[i]
             str1=files[i]
-            print str1
-            if validate(str1):                
+            print "processing: "+str1
+            if validate(str1): 
+                '''
+                1. check if there is already a folder could have this story
+                    - yes, put it in
+                    - no, go to 2.
+                2. check if it has common prefix with next story 
+                    - yes, create a folder and put two stories in.
+                    - no, move on
+                '''
+                found=[]
                 if len(folders)>0:
                     # print folders
-                    found=filter(lambda x:str1.startswith(x.encode('utf8')),folders)
-                    if len(found)>0:
-                        dst=os.path.join(path,found[0].encode('utf8'))
-                        filepath=os.path.join(dst,str1)
-                        if not os.path.exists(filepath):
-                            shutil.move(str1,dst)
-                            continue
-                if i<file_count-1:   
-                    str2=files[i+1]
-                    if validate(str2):
-                        comm_str=findCommStr(str1,str2)
-                        if comm_str!=None:
-                            print 'comm:'+comm_str
-                            folders.append(comm_str)
-                            folder_path=os.path.join(path,comm_str.encode('utf8'))
-                            if not os.path.exists(folder_path):
-                                print "创建文件夹: "+folder_path
-                                os.mkdir(folder_path)
+                    found=filter(lambda x:str1.startswith(x.encode('utf8')),folders)    
+                if len(found)>0:
+                    dst=os.path.join(path,found[0].encode('utf8'))
+                    filepath=os.path.join(dst,str1)
+                    if not os.path.exists(filepath):
+                        shutil.move(str1,dst)
+                else:        
+                    if i<file_count-1:   
+                        str2=files[i+1]
+                        if validate(str2):
+                            comm_str=findCommStr(str1,str2)
+                            if comm_str!=None:
+                                folders.append(comm_str)
+                                folder_path=os.path.join(path,comm_str.encode('utf8'))
+                                if not os.path.exists(folder_path):
+                                    print "创建文件夹: "+folder_path
+                                    os.mkdir(folder_path)
                                 shutil.move(str1,folder_path)
                                 shutil.move(str2,folder_path)
+                                i+=1
             i+=1
 
     
